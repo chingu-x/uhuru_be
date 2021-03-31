d441d58919fb2ef45cd34513fedf7d706f65427b
Jim Medlock <jdmedlock@gmail.com>
2021-03-30 20:30:34 -0500

    Feature: Display port number on startup

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const fastify = await buildApp()
 
 try {
   await fastify.listen(process.env.PORT || 8080)
+  console.log("Listening on port: ", process.env.PORT || 8080)
 } catch (error) {
   fastify.log.error(error)
   process.exit(1)
