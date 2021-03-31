c125c719d93255c0292b8e3923883c13c565d60b
Jim Medlock <jdmedlock@gmail.com>
2021-03-30 20:24:16 -0500

    Feature: Update port setting logic

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ dotEnv.config()
 const fastify = await buildApp()
 
 try {
-  await fastify.listen(process.env.SERVER_PORT)
+  await fastify.listen(process.env.PORT || 8080)
 } catch (error) {
   fastify.log.error(error)
   process.exit(1)
