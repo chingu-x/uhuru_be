e8d6f014ba1d61f176ff4eb81216d28314b31fda
jdmedlock <jdmedlock@gmail.com>
2021-04-14 12:10:27 -0500

    Feature: Format classmate email body

--- src/controllers/mailjet.js
+++ src/controllers/mailjet.js
@@ -27,8 +27,41 @@ export const sendMessage = async (request, reply) => {
             }
           ],
           "Subject": `CHS73 - New comment from ${ request.body.name }`,
-          "TextPart": `${ request.body.message }`,
-          "HTMLPart": `<h3>Email from: ${ request.body.name }</h3><h3>Email address: (${ request.body.from })</h3><p>${ request.body.message }</p>`,
+          "TextPart": `
+            Email from: ${ request.body.name } Email address: ${ request.body.from }\r\n \
+            Street: ${ request.body.street }\r\n \
+            City: ${ request.body.city } State: ${ request.body.state } Zip: ${ request.body.zipcode }\r\n \
+            Phone: ${ request.body.phone }
+            Volunteer to help: ${ request.body.volunteer }\r\n \
+            Message from classmate:\r\n ${ request.body.message }
+          `,
+          "HTMLPart": ` \
+            <div style=\"font-weight: bold; font-size: medium;\">Email from: \
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.name } </span>\
+              <span style=\"font-weight: bold; font-size: medium;\">Email address: </span> \
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.from }</span> \
+            </div> \
+            <div style=\"margin-top: .25rem; font-weight: bold; font-size: medium;\">Street: \
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.street }</span> \
+            </div> \
+            <div style=\"margin-top: .25rem; font-weight: bold; font-size: medium;\">City: \
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.city } </span> \
+              <span style=\"font-weight: bold; font-size: medium;\">State: </span>\
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.state }</span> \
+              <span style=\"font-weight: bold; font-size: medium;\">Zip: </span>\
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.zipcode }</span> \
+            </div> \
+            <div style=\"margin-top: .25rem; font-weight: bold; font-size: medium;\">Phone: \
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.phone }</span> \
+            </div> \
+            <div style=\"margin-top: .25rem; font-weight: bold; font-size: medium;\">Volunteer to help:  \
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.volunteer }</span>
+            </div>\ 
+            <div style=\"margin-top: 1rem; font-weight: bold; font-size: medium;\">Message from classmate:</div>\ 
+            <div style=\"margin-top: .25rem; font-size: medium;\">
+              <span style=\"font-weight: normal; font-size: medium;\">${ request.body.message }</span> \
+            </div> \
+          `,
         }
       ]
     })
